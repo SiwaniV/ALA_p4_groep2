94436600f40f23a53dda13682780911645888405
SiwaniV <74538186+SiwaniV@users.noreply.github.com>
2021-05-20 16:21:29 +0200

    javascript toegevoegd van eenvoudige rekenpagina

---
+++ eenvoud.js
@@ -0,0 +1,44 @@
+//variabelen hier
+var locateAni; //locatie van animatie
+var posAni = 0; //positie 
+var setVar; //set interval variabele
+
+
+
+
+
+window.onload = function() {
+
+  /*button.onclick = function(){
+    
+  }*/
+
+  
+
+}
+
+//functions hier
+
+function moveBlock(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
