284d9b6d15b7eed899568f0faf132f4ee1a8741e
SiwaniV <6003716@mborijnland.nl>
2021-06-15 12:07:52 +0200

    eenvoudig rekenen aanpassen

--- JS/eenvoud.js
+++ JS/eenvoud.js
@@ -8,11 +8,11 @@ window.onload = function(){
     
 
 
-    function tafelReeks() {
+    function genereerTafel() {
         var vermenigvuldigtal = document.getElementById("tafel-vermenigvuldigtal").value; // De invoer van de gebruiker
         var maxVermenigvuldiger = document.getElementById("tafel-max-vermenigvuldiger").value;
-        var antwoordVeld = document.getElementById("tafels");
-
+        var antwoordVeld = document.getElementById("tafel");
+    
         antwoordVeld.value = ""; // We beginnen elke reeks met een leeg antwoord veld
     
         for(var vermenigvuldiger = 1; vermenigvuldiger <= maxVermenigvuldiger; vermenigvuldiger++) {
@@ -21,7 +21,7 @@ window.onload = function(){
             antwoordVeld.value += vermenigvuldiging + "\n"; // We de berekening toe aan het antwoord veld afgesloten met een nieuwe regel \n
         }
     
-       
+
     }
     
     
