a82d700275d8764d9cabfc8b863a86d62adde115
SiwaniV <6003716@mborijnland.nl>
2021-06-15 12:43:49 +0200

    header php bewerkt

--- _header.php
+++ _header.php
@@ -20,10 +20,10 @@
 
     <article id="log">
         <?php if(isset($_SESSION['ingelogd']) && $_SESSION['ingelogd'] == true) { ?>
-        <br><br><a href="uitloggen.php" onclick="return confirm('Ben je zeker dat je je wilt afmelden?')">Uitloggen</a></p>
+            <p></p><a href="uitloggen.php" onclick="return confirm('Ben je zeker dat je je wilt afmelden?')">Uitloggen</a></p>
         <?php }else { ?>
-            <p><a href="inloggen.php">Inloggen</a></li><br></p>
-            <p><a href="aanmelden.php">Aanmelden</a></li></p>
+            <p><a href="inloggen.php">Inloggen</a></p>
+            <p><a href="aanmelden.php">Aanmelden</a></p>
         <?php } ?>
     </article>
 </header>
